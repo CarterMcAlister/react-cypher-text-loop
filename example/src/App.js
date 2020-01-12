adbf697c3fc989c42a2233005452db2a9c3f7a7a
Carter <carter@rocketmiles.com>
2020-01-12 13:35:06 -0800

    Style example page

--- example/src/App.js
+++ example/src/App.js
@@ -6,9 +6,59 @@ import CypherText from 'react-cypher-text'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <CypherText textList={['React', 'Cypher', 'Text']} />
-      </div>
+      <Wrapper>
+        <Container>
+          <CypherTextStyled
+            textList={['React', 'Cypher', 'Text', 'Loop']}
+            placeholderColor="#000"
+          />
+        </Container>
+
+        <Container>
+          <LinkButton
+            className="link"
+            target="_blank"
+            href="https://github.com/CarterMcAlister/react-cypher-text-loop"
+          >
+            View on GitHub
+          </LinkButton>
+          <LinkButton
+            className="link"
+            target="_blank"
+            href="https://www.npmjs.com/package/react-cypher-text-loop"
+          >
+            View on NPM
+          </LinkButton>
+        </Container>
+      </Wrapper>
     )
   }
 }
+
+const CypherTextStyled = styled(CypherText)`
+  color: #fff;
+  font-size: 3rem;
+`
+
+const Wrapper = styled.div`
+  background: #373f51;
+  height: 80vh;
+  padding-top: 20vh;
+  font-family: 'Courier New', Courier, monospace;
+`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`
+const LinkButton = styled.a`
+  background-color: #f6f9fc;
+  color: black;
+  padding: 1em 1.5em;
+  text-decoration: none;
+  margin: 10px;
+  border-radius: 0.25rem;
+  min-width: 140px;
+  display: flex;
+  justify-content: center;
+`
